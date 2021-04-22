8a63552a6d6638cfeb8590ac2491150459ed5b55
Coda759 <45927826+Coda759@users.noreply.github.com>
2021-04-22 16:42:27 -0400

    Notebook Creation barebones page done

--- app/src/main/java/csc248/smirn42/NotebookScheduler/AddNotebook.java
+++ app/src/main/java/csc248/smirn42/NotebookScheduler/AddNotebook.java
@@ -1,13 +1,34 @@
 package csc248.smirn42.NotebookScheduler;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddNotebook extends AppCompatActivity {
 
+    RadioGroup radioGroup;
+    RadioButton note, checklist;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.notebook_creation);
+
+        radioGroup = findViewById(R.id.radio_group);
+        note = findViewById(R.id.note_selection);
+        checklist = findViewById(R.id.checklist_selection);
+
+    }
+
+    public void checkInput(View view) {
+        if(note.isChecked()) {
+            System.out.println("Notes");
+        } else {
+            System.out.println("Checklist");
+        }
     }
 }
