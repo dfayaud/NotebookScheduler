4f9205c2680483f0df24f6c411f548b6f263a3bc
Christopher <cbaumann@student.sjcny.edu>
2021-04-27 05:54:19 -0400

    Created the settings page

---
+++ app/src/main/java/csc248/smirn42/NotebookScheduler/Settings.java
@@ -0,0 +1,14 @@
+package csc248.smirn42.NotebookScheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
